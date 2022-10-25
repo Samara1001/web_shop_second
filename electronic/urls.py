8fd80c2e85766eaf4f5c7422e6dd53a2d7821886
Abdurashit <tashit8ma@gmail.com>
2022-10-25 18:05:00 +0600

    somethinng

--- electronic/urls.py
+++ electronic/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index, name='home'),
+    path('main', views.main, name='about'),
     path('main', views.main, name='about')
 ]
