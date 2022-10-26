fc69547e6766b88dfa2f7fc426ec988732e0c1ea
Samara1001 <smambetova77@gmail.com>
2022-10-26 18:32:04 +0600

    something

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,15 @@ from electronic import apiView
 
 
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('electronic/', include('electronic.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='electronic'))
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-]
