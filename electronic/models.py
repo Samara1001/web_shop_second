8034acaad9638ae82003a8ebb8016cd959d2d4c8
shatman1 <shatmanbularov@gmail.com>
2022-10-31 17:47:42 +0600

    add img

--- electronic/models.py
+++ electronic/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Electronic(models.Model):
     name = models.CharField(max_length=40, verbose_name='Имя')
     brand = models.CharField(max_length=30, verbose_name='Марка')
+    img = models.ImageField(upload_to='electronic', null=True, blank=True)
     price = models.FloatField(verbose_name='Цена')
     description = models.TextField(verbose_name='Описание')
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
